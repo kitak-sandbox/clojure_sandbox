083606ce22600f6eb53a8077052fc89503420bcb
Keisuke KITA <kei.kita2501@gmail.com>
2014-10-09 08:28:56 +0900

    clojurescriptでhello world

---
+++ cljs-sample/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljs-sample
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ cljs-sample/test/cljs_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljs-sample.core-test
+  (:require [clojure.test :refer :all]
+            [cljs-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
